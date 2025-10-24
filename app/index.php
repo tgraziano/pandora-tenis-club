6f457ae2ba46ea3c32d6066a33c23039d9def812
tgraziano <tomas.graziano@davinci.edu.ar>
2025-10-23 21:04:56 -0300

    chore: add prettier config and global CSS styles with normalize

--- app/index.php
+++ app/index.php
@@ -9,17 +9,19 @@ $saludo = "Hola mundo";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Pandora tenis club</title>
+  <title>Club Pandora</title>
   <meta name="description" content="Club de Tenis Pandora">
   <meta name="keywords" content="tenis, club, pandora, padel, argentina, buenos aires, caba, ciudad autonoma de buenos aires, club de tenis, club de padel, club de tenis caba, club de padel caba">
   <meta name="author" content="Tomas Graziano">
   <meta name="author" content="Juan Cruz">
   <meta name="robots" content="index, follow">
   <link rel="icon" href="/favicon.svg">
+  <link rel="stylesheet" href="node_modules/modern-normalize/modern-normalize.css">
+  <link rel="stylesheet" href="/assets/global.css">
 </head>
 
 <body>
-  <h1><?php echo $saludo; ?></h1>
+  <h1>Club Pandora</h1>
 </body>
 
 </html>
